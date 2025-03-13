2f5941e2755a17939adcb4a90f46b2aef3bc6c88
bahram_adra <iammhosseini@gmail.com>
2025-03-13 15:49:32 +0330

    fix container bugs

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Container({children}) {
   return (
-    <div className='container mx-auto px-5 sm:px-10 xl:px-20 lg:px-20'  >
+    <div className='container mx-auto  px-5 sm:px-10 md:px-5 xl:px-20 lg:px-15'  >
       {children}
     </div>
   )
