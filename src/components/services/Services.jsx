84b8bb8f219caae572bb50b1146b33dbc9af5753
bahram_adra <iammhosseini@gmail.com>
2025-03-16 19:21:37 +0330

    complite services component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -2,15 +2,13 @@ import React from 'react'
 import Container from '../container/container'
 import styled from "./services.module.css"
 import ServicesItem from '../servicesItem/servicesItem'
+import SectionHeader from '../sectionHeader/SectionHeader'
 
 function Services() {
   return (
     <section className={`${styled.serviceSection} my-30 text-sky-50`}>
         <Container>
-            <div className={`${styled.sectionHeader} flex justify-center flex-col mb-15 items-center text`}>
-                <h2 className={`${styled.sectionTitle} font-bold mb-10`} > My Quality Services</h2>
-                <p className='font-semibold max-w-[800px] text-center'>We put your ideas and thus your wishes in the form of a unique web project that inspires you and you customers</p>
-            </div>
+            <SectionHeader title="My Quality Services" description="We put your ideas and thus your wishes in the form of a unique web project that inspires you and you customers "></SectionHeader>
 
             <div className={`${styled.servicesWidget} relative`}>
                 <ServicesItem number={"1"} title={"Responsive design"} content={"I break down complex user experinece problems to create integritiy focussed solutions that connect billions of people"}/>
