c9fd8a493ec7ec0f40d367c623e713e858191711
bahram_adra <iammhosseini@gmail.com>
2025-03-23 07:15:10 +0330

    footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,29 +1,35 @@
 import React from 'react'
 import Container from '../container/container'
+import { Link } from 'react-router'
+import styled from "./footer.module.css"
 
 function Footer() {
   return (
-    <>
+    <div className={`${styled.footerArea} py-10`}>
         <Container>
-          <div className=' '>
-            <h1>footer</h1>
-            <h1>footer</h1>
-            <h1>footer</h1>
-            <h1 className='text-green-600'>footer</h1>
-            <h1 className='text-green-600'>footer</h1>
-            <h1 className='text-green-600 text-2xl'>footer</h1>
-            <h1 className='text-green-600 text-2xl'>footer</h1>
-            <h1 className='text-green-600 text-2xl'>footer</h1>
-            <h1 className='text-green-600 text-2xl'>footer</h1>
-            <h1 className='text-green-600 text-2xl'>footer</h1>
-            <h1 className='text-green-600 text-2xl'>footer</h1>
-            <h1 className='text-green-600 text-2xl'>footer</h1>
+          <div className='flex justify-between text-sky-50'>
+            <div className={`log-box flex items-center w-70`}>
+              <img src="https://img.icons8.com/color-pixels/32/twitter.png" alt="" />
+            </div>
+
+            <div className={`footer-menu w-120`}>
+              <ul className={`grid sm:grid-cols-2 gap-5 `}>
+                  <li > <Link to="/"> Home </Link></li>
+                  <li> <Link to="/services"> Services </Link></li> 
+                  <li> <Link to="/works"> Works </Link></li> 
+                  <li> <Link to="/skills"> Skills </Link></li> 
+                  <li> <Link to="/contact"> Contact</Link></li>  
+              </ul>
+            </div>
+
+            <div className={`copy-text text-theme-primary-500 font-semibold flex items-center`}>
+              <p>&copy; 2025 All rights reserved by Him</p>
+            </div>
           </div>
-            
+           
         </Container>
       
-    </>
-  )
-}
+    </div>
+)}
 
 export default Footer
